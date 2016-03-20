ca546c8b56196938461b87e5098fbe148f69298e
Zach Strenfel <zwstrenfel@gmail.com>
2016-03-20 15:04:31 -0700

    updated serer

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path                = require('path');
 
 // Environment ------------------
 var env = process.env.NODE_ENV || 'development';
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 
 // Express Setup ----------------
 var app = express();
@@ -25,5 +25,6 @@ app.get('/', function(req, res) {
 // Launch app -------------------
 //app.listen(port);
 app.listen(port);
+//app.listen(port, '10.134.0.227');
 console.log("*** server running");
 exports = module.exports = app;
